dd822b13b85ac9976b9838f67782aa4f45aa2c52
Trouni Tiet <trouni@gmail.com>
2022-02-23 21:08:19 +0900

    add find_packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="whosnext3000",
     version="0.1.0",
     author_email="trouni@kesseo.com",
     description="Spinning wheel utility to randomly pick a candidate.",
-    packages=["whosnext3000"],
+    packages=find_packages(),
     install_requires=["simple_term_menu"],
     scripts=["bin/whosnext3000"],
 )
